4c8ca4100cbaff6d30106bf6c5e36e3f8dd07ee8
ohsumin <otnals25@gmail.com>
2024-08-08 01:10:36 +0900

    동적 페이지 메타데이터 변경

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,8 +1,7 @@
 import { API_URL } from "../app/(home)/page";
 import movieStyle from "../styles/movie-info.module.css"
 
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
